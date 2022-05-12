7f0616753b80d179dff833491f7ed79090dde59f
Jie <jie985563349@outlook.com>
2022-05-12 17:59:58 +0800

    chore: :tada:Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mantine/core';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Artboard</h1>
+      <Button>Hello Artboard!</Button>
+    </div>
+  );
+}
+
+export default App;
