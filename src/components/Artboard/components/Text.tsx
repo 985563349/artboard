b33681eeff4ee3c48b25ee3a8e5b7a318e70b255
Jie <jie985563349@outlook.com>
2022-05-26 16:23:12 +0800

    feat: add transformer component

--- src/components/Artboard/components/Text.tsx
+++ src/components/Artboard/components/Text.tsx
@@ -1,10 +1,15 @@
 import type { ComponentProps, FC } from 'react';
 import { Text as KonvaText } from 'react-konva';
+import Transformer from './Transformer';
 
 export type TextProps = ComponentProps<typeof KonvaText>;
 
 const Text: FC<TextProps> = (props) => {
-  return <KonvaText {...props} />;
+  return (
+    <Transformer selected>
+      <KonvaText {...props} />
+    </Transformer>
+  );
 };
 
 export default Text;
