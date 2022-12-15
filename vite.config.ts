bd37ba41e9a3d4a8f8d6d3807d4f0723a2e6c5ea
王杰 <jie985563349@outlook.com>
2022-12-15 19:44:22 +0800

    chore: upgrade to vite 4.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 import { join } from 'path';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
